78fe1e5864ac3360240d20917fb8c0451e7afb6c
stasi815 <anastasia.gallardo@students.makeschool.com>
2019-12-13 17:16:20 -0800

    fixed admin tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -7,12 +7,12 @@ from django.contrib.auth.models import User
 
 class UserTestCase(TestCase):
 
-    def test_good_login(self):
+    def test_success_login(self):
         user = User.objects.create_user("user", password="password")
         auth_user = authenticate(username="user", password="password")
         self.assertTrue(auth_user is not None)
         self.assertEqual(user.pk, auth_user.pk)
 
-    def test_bad_login(self):
+    def test_fail_login(self):
         authorized_user = authenticate(username="faker", password="password")
         self.assertTrue(authorized_user is None)
